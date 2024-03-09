71ca89ba1941389f585313157d3dc38d1b2dc3f2
mahmoud-mqld <mqldm@yahoo.com>
2024-03-10 00:20:30 +0200

    Configure React app for deployment to GitHub Pages

--- src/App.js
+++ src/App.js
@@ -27,7 +27,7 @@ export default function App() {
 
   let queryClient = new QueryClient()
 
-  let Routes = createHashRouter([
+  let Routes = createBrowserRouter([
     {
       path: "", element: <Layout />, children: [
         { path: "home", element: <ProtectingRouting><Home /></ProtectingRouting> },
